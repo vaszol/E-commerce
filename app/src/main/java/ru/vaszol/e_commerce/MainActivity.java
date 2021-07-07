68e262e2f47b344c24f997d1d5bce8404aecc7a2
vasiliy <vaszol@mail.ru>
2021-07-07 23:38:06 +0300

    Добавил список категорий

--- app/src/main/java/ru/vaszol/e_commerce/MainActivity.java
+++ app/src/main/java/ru/vaszol/e_commerce/MainActivity.java
@@ -1,14 +1,41 @@
 package ru.vaszol.e_commerce;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import ru.vaszol.e_commerce.adapter.CategoryAdapter;
+import ru.vaszol.e_commerce.model.Category;
 
 import android.os.Bundle;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView categoryRecycler;
+    private CategoryAdapter categoryAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<Category> categoryList = new LinkedList<>();
+        categoryList.add(new Category(1,"Игры"));
+        categoryList.add(new Category(1,"Сайты"));
+        categoryList.add(new Category(1,"Языки"));
+        categoryList.add(new Category(1,"Прочее"));
+
+        setCategoryRecycler(categoryList);
+    }
+
+    private void setCategoryRecycler(List<Category> categoryList) {
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
+        categoryRecycler = findViewById(R.id.categoryRecycler);
+        categoryRecycler.setLayoutManager(layoutManager);
+
+        categoryAdapter = new CategoryAdapter(this, categoryList);
+        categoryRecycler.setAdapter(categoryAdapter);
     }
 }
---
+++ app/src/main/java/ru/vaszol/e_commerce/adapter/CategoryAdapter.java
@@ -0,0 +1,53 @@
+package ru.vaszol.e_commerce.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import ru.vaszol.e_commerce.R;
+import ru.vaszol.e_commerce.model.Category;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder> {
+
+    private Context context;
+    private List<Category> categories;
+
+    public CategoryAdapter(Context context, List<Category> categories) {
+        this.context = context;
+        this.categories = categories;
+    }
+
+    @NonNull
+    @Override
+    public CategoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View categoryItems = LayoutInflater.from(context).inflate(R.layout.category_item, parent, false);
+        return new CategoryViewHolder(categoryItems);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CategoryAdapter.CategoryViewHolder holder, int position) {
+        holder.categoryTitle.setText(categories.get(position).getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return categories.size();
+    }
+
+    public static final class CategoryViewHolder extends RecyclerView.ViewHolder {
+
+        TextView categoryTitle;
+
+        public CategoryViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            categoryTitle = itemView.findViewById(R.id.categoryTitle);
+        }
+    }
+}
